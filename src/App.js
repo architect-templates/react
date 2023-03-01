776aa1f8585b1f5caa47384c77f346ac37ba69fd
DevMandy <devmandy@yahoo.com>
2023-03-01 17:57:28 -0600

    Remove header, uncomment form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import ArchitectHeader from './components/ArchitectHeader';
-// import ArchitectItems from "./components/ArchitectItems";
-import ArchitectNextSteps from "./components/ArchitectNextSteps";
+import ArchitectItems from "./components/ArchitectItems";
+
 import './index.css';
 
 const App = () => (
   <div className="container">
     <ArchitectHeader/>
-    <ArchitectNextSteps/>
-    {/* <ArchitectItems/> */}
+    { <ArchitectItems/> }
   </div>
 );
 
 export default App;
 
-/*
-  Some lines of code in this file are commented out. Follow along with the
-  "Registering Dependencies" section in the README to learn more about enabling
-  some of the additional features included in this application.
-*/
